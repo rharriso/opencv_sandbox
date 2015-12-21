ea6cf879dcf1bd3bd62b9b902bf631ba2dde1493
Ross Harrison <rtharrison86@gmail.com>
2015-12-21 15:51:12 -0500

    mat / point basics

--- load_modify_save.cpp
+++ load_modify_save.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/types_c.h>
---
+++ mat.cpp
@@ -0,0 +1,31 @@
+//
+// Created by rharriso on 21/12/15.
+//
+#include <iostream>
+#include <algorithm>
+#include <random>
+#include <chrono>
+#include <opencv2/core/core.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(){
+    // using built in array pixel types
+    auto M = Mat(2,2, CV_8UC3, Scalar(0, 0, 255));
+    cout << "M: " << M << endl << endl;
+
+    // using c++ array to create 3d matrix of 3 value pixels
+    int sz[3] = {2,2,2};
+    auto L = Mat(3, sz, CV_8UC(3), Scalar(0, 0, 255));
+
+    // vector of 3d points
+    srand(time(0));
+    auto points = vector<Point3d>(20);
+    generate(points.begin(), points.end(), [](){
+        return Point3d(rand() % 256, rand() % 256, rand() % 256);
+    });
+    cout << "Vector of 3d points" << points << endl << endl;
+
+    return 0;
+}
